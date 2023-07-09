586a7c17947c943e835fc0bca5aff0e89d7d5cbe
vsaraikin <vlad.saraykin@mail.ru>
2023-07-09 14:05:57 +0300

    create schema while starting app

--- keitaro/src/main/java/com/quatex/evaproxy/keitaro/config/KeitaroAutoConfiguration.java
+++ keitaro/src/main/java/com/quatex/evaproxy/keitaro/config/KeitaroAutoConfiguration.java
@@ -1,7 +1,20 @@
 package com.quatex.evaproxy.keitaro.config;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @Configuration
 public class KeitaroAutoConfiguration {
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        final ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("db/schema.sql")));
+        return initializer;
+    }
 }
--- keitaro/src/main/resources/db/schema.sql
+++ keitaro/src/main/resources/db/schema.sql
@@ -7,4 +7,4 @@ create table IF NOT EXISTS partner_event (
     created timestamp without time zone,
     last_change_updated timestamp without time zone
     );
-create unique index partner_event_click_id_key on partner_event using btree (click_id);
+create unique index IF NOT EXISTS partner_event_click_id_key on partner_event using btree (click_id);
